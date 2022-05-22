3d507de86404afc1addda7f3a4cdc80f9ffd9f9e
JoseGc7 <53314492+JoseGc7@users.noreply.github.com>
2022-05-22 02:30:13 -0500

    Creación de dependecia propia

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/component/ComponentTwoImplement.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentos.component;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentTwoImplement implements ComponentDependency{
+
+    @Override
+    public void saludar() {
+        System.out.println("Hola Mundo desde mi componente 2 :)");
+    }
+}
